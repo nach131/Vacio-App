02f7b1360bc8b8fbe816af5f127c78ec75679b75
nach131 <nach.mota@gmail.com>
2020-09-09 15:49:50 +0200

    Separa animacion en componente

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import './App.css';
-import { bounceInDown } from 'react-animations';
-import Radium, { StyleRoot } from 'radium';
+import Anima from './componets/Anima'
 
 
 
-const styles = {
-  bounceInDown: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(bounceInDown, 'bounceInDown')
-  }
-}
+
+
+
 
 class App extends React.Component {
   render() {
     return (
+      <Anima />
 
-      <StyleRoot>
-        <div className="test" >
-          <div  className="App">
-            <header className="App-header">
-              <h1 style={styles.bounceInDown} >Quitado</h1>
-
-            </header>
-          </div>
-        </div>
-      </StyleRoot>
     );
   }
 }
---
+++ src/componets/Anima.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import { bounceInDown } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+const styles = {
+  bounceInDown: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(bounceInDown, 'bounceInDown')
+  }
+}
+
+class Anima extends Component {
+  // state = {  }
+  render() { 
+    return ( 
+      <StyleRoot>
+        <div className="test" >
+          <div  className="App">
+            <header className="App-header">
+              <h1 style={styles.bounceInDown} >Quitado</h1>
+
+            </header>
+          </div>
+        </div>
+      </StyleRoot>
+     );
+  }
+}
+ 
+export default Anima;
