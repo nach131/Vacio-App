b4c028742ad34e841e398313f3d717242355b762
nach131 <nach.mota@gmail.com>
2020-09-09 20:11:03 +0200

    Insertar varios items en mongoDB

--- src/App.js
+++ src/App.js
@@ -24,26 +24,32 @@ const foto = new Foto ({
   "url": "https://apod.nasa.gov/apod/image/0001/flammarion_halfcolor_big.gif"
 });
 
-foto.save();
+// foto.save();
 
-
-
-    // {
-    //   "date": "2000-01-02",
-    //   "explanation": "There, that faint dot in the center - that's the largest rock known.  It is larger than every known asteroid, moon, and comet nucleus.  It is larger than any other rocky planet.  (Nobody knows for sure what size rocks lie at the cores of Jovian planets, or orbit other stars.)  The Voyager 1 spacecraft took this picture in 1990 from the outer Solar System.  This rock is so large its gravity makes it nearly spherical, and holds heavy gases near its surface.  Yesterday, this rock started another orbit around its parent star, for roughly the 5 billionth time, spinning over 350 times during each trip.  Happy Gregorian Calendar New Year to all the human inhabitants of this rock we call Earth.",
-    //   "hdurl": "https://apod.nasa.gov/apod/image/0001/earth_vg1.jpg",
-    //   "media_type": "image",
-    //   "service_version": "v1",
-    //   "title": "The Largest Rock Known",
-    //   "url": "https://apod.nasa.gov/apod/image/0001/earth_vg1_big.jpg"
-    // },
-    // {
-    //   "date": "2000-01-03",
-    //   "explanation": "The complex shell of a star seen to explode 300 years ago is helping astronomers to understand how that star exploded.  The above recently released image of supernova remnant Cassiopeia A (Cas A) shows unprecedented detail in three X-ray colors.  The relationship between brightness, color, and position of material in the image indicates where in the star this material was just before the explosion.  Bright knots on the left, for example, contain little iron, and so are hypothesized to originated from a higher layer than outer red filaments, which are iron rich.  The blue region on the right is seen through absorbing dust,  and so appears depleted of low-energy X-rays.  It takes light ten years to cross the gas shell of the Cas A supernova remnant, which is 10,000 light-years distant. Most of the elements that make people and planets were produced in supernova explosions.",
-    //   "hdurl": "https://apod.nasa.gov/apod/image/0001/casa_chandra_big.jpg",
-    //   "media_type": "image",
-    //   "service_version": "v1",
-    //   "title": "Cas A Supernova Remnant in X-Rays",
-    //   "url": "https://apod.nasa.gov/apod/image/0001/casa_chandra.jpg"
-    // }
-
+var arr = [
+    {
+      "date": "2000-01-02",
+      "explanation": "There, that faint dot in the center - that's the largest rock known.  It is larger than every known asteroid, moon, and comet nucleus.  It is larger than any other rocky planet.  (Nobody knows for sure what size rocks lie at the cores of Jovian planets, or orbit other stars.)  The Voyager 1 spacecraft took this picture in 1990 from the outer Solar System.  This rock is so large its gravity makes it nearly spherical, and holds heavy gases near its surface.  Yesterday, this rock started another orbit around its parent star, for roughly the 5 billionth time, spinning over 350 times during each trip.  Happy Gregorian Calendar New Year to all the human inhabitants of this rock we call Earth.",
+      "hdurl": "https://apod.nasa.gov/apod/image/0001/earth_vg1.jpg",
+      "media_type": "image",
+      "service_version": "v1",
+      "title": "The Largest Rock Known",
+      "url": "https://apod.nasa.gov/apod/image/0001/earth_vg1_big.jpg"
+    },
+    {
+      "date": "2000-01-03",
+      "explanation": "The complex shell of a star seen to explode 300 years ago is helping astronomers to understand how that star exploded.  The above recently released image of supernova remnant Cassiopeia A (Cas A) shows unprecedented detail in three X-ray colors.  The relationship between brightness, color, and position of material in the image indicates where in the star this material was just before the explosion.  Bright knots on the left, for example, contain little iron, and so are hypothesized to originated from a higher layer than outer red filaments, which are iron rich.  The blue region on the right is seen through absorbing dust,  and so appears depleted of low-energy X-rays.  It takes light ten years to cross the gas shell of the Cas A supernova remnant, which is 10,000 light-years distant. Most of the elements that make people and planets were produced in supernova explosions.",
+      "hdurl": "https://apod.nasa.gov/apod/image/0001/casa_chandra_big.jpg",
+      "media_type": "image",
+      "service_version": "v1",
+      "title": "Cas A Supernova Remnant in X-Rays",
+      "url": "https://apod.nasa.gov/apod/image/0001/casa_chandra.jpg"
+    }
+  ]
+Foto.insertMany(arr, function(error){
+  if (error) {
+    console.log(error)
+  } else {
+    console.log("Se añadieron items nuevos")
+  }
+})
