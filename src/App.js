61346a4052e271aab2b3088cc4767a0ed1a49393
nacho <nacho.mota@gmail.com>
2021-07-24 20:25:16 +0200

    INICIO nada

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import './App.css';
-import { bounceInDown } from 'react-animations';
-import Radium, { StyleRoot } from 'radium';
 
 
-
-const styles = {
-  bounceInDown: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(bounceInDown, 'bounceInDown')
-  }
-}
 
 class App extends React.Component {
   render() {
     return (
 
-      <StyleRoot>
-        <div className="test" >
-          <div  className="App">
-            <header className="App-header">
-              <h1 style={styles.bounceInDown} >Quitado</h1>
+      <div className="App">
+        <header className="App-header">
+          <h2>Nada</h2>
 
-            </header>
-          </div>
-        </div>
-      </StyleRoot>
+        </header>
+      </div>
     );
   }
 }
