a0a120d3c7a63b7cc586989939af15d16b97e741
nacho <nacho.mota@gmail.com>
2021-09-14 21:00:00 +0200

    solo el fetch data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { useState } from 'react';
 import './App.css';
+import { makeData } from "./Utils";
+
+// Import React Table
+import ReactTable from "react-table";
+// import "react-table/react-table.css";
+
+function App () {
+
+  const [data, setData] = useState(makeData())
 
 
-
-function App() {
-
-  const [appState, changeState] = useState({
-    activeObject: null,
-    objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 },]
-  })
-
-
-  function toggleActive(index) {
-    changeState({ ...appState, activeObject: appState.objects[index] })
-  }
-
-  function toggleActiveStyles(index) {
-    if (appState.objects[index] === appState.activeObject) {
-      return "box active"
-    } else {
-      return "box inactive"
-    }
-  }
-
+  console.log(data)
 
   return (
-
-    <div className="App" >
-      {appState.objects.map((elements, index) => (
-        <div key={index} className={toggleActiveStyles(index)} onClick={() => {
-          toggleActive(index)
-        }}>
-
-        </div>
-      ))}
-    </div >
-  )
-
+    <div>
+      <ReactTable
+        data={data}
+        columns={[
+          {
+            Header: "Name",
+            columns: [
+              {
+                Header: "First Name",
+                accessor: "firstName"
+              },
+              {
+                Header: "Last Name",
+                id: "lastName",
+                accessor: d => d.lastName
+              }
+            ]
+          },
+          {
+            Header: "Info",
+            columns: [
+              {
+                Header: "Age",
+                accessor: "age"
+              }
+            ]
+          }
+        ]}
+        defaultPageSize={20}
+        style={{
+          height: "400px" // This will force the table body to overflow and scroll, since there is not enough room
+        }}
+        className="-striped -highlight"
+      />
+      <br />
+      {/* <Tips /> */}
+      {/* <Logo /> */}
+    </div>
+  );
 }
+
+
 
 
 export default App
