029961d289a88741f8ddc30f3fdc5fb14c269a84
nacho <nacho.mota@gmail.com>
2021-10-09 20:50:59 +0200

    ok

--- src/Circlebox.js
+++ src/Circlebox.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function Circlebox () {
 
@@ -8,6 +9,19 @@ function Circlebox () {
     console.log(e.target.value)
     setColor(e.target.value)
   }
+  const type = 137
+  const address = '0x353DD645CbD1777CAb993170291Ca310c9E2DB4D'
+
+  const config = {
+    method: 'get',
+    url: `https://api.covalenthq.com/v1/${type}/address/${address}/balances_v2/?&key=ckey_ff9e70de0492454f9c74796b73d`,
+    headers: {}
+  }
+
+  axios(config)
+    .then(d => console.log(d.data.data.items))
+
+  // console.log(dato)
 
   return (
     <div>
