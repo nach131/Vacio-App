c72178ed39b921b89a2308ebb0ca542cc4c3ad4b
nacho <nacho.mota@gmail.com>
2021-12-28 21:27:53 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
-import React from 'react';
-import './App.css';
-import { bounceInDown } from 'react-animations';
-import Radium, { StyleRoot } from 'radium';
-import { FaRocket } from 'react-icons/fa';
-import { BsFillEggFill,BsEgg } from "react-icons/bs";
+import React, { useEffect, useState } from 'react';
+function App () {
+  const [price, setPrice] = useState();
+  const [autoRefresh, setAutoRefresh] = useState(true);
 
-const styles = {
-  bounceInDown: {
-    animation: 'x 1s',
-    animationName: Radium.keyframes(bounceInDown, 'bounceInDown')
-  }
+  useEffect(() => {
+    let intervalo
+    const fechData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.coincap.io/v2/assets/bitcoin"
+        )
+        const result = await response.json()
+        const btcPrice = (+result?.data?.priceUsd).toFixed(2)
+        setPrice(btcPrice)
+      } catch (error) {
+        console.log("error", error)
+      }
+    }
+    // Si el precio esta vacio , get precio btc
+    if (!price) {
+      fechData()
+    }
+
+    if (autoRefresh) {
+      intervalo = setInterval(() => {
+        fechData()
+      }, 5 * 1000)
+    }
+
+    return () => {
+      clearInterval(intervalo)
+    }
+
+  }, [autoRefresh, price])
+
+  return (
+    <div className="App">
+      <h2>{price && `Bitcoin Price: $${price}`}</h2>
+      <div className="refresh">
+        <div className="refresh-label">Auto refresh:</div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={autoRefresh}
+            onChange={(e) => {
+              setAutoRefresh(e.target.checked);
+            }}
+          />
+          <span className="slider round"></span>
+        </label>
+      </div>
+    </div>
+  )
 }
-
-class App extends React.Component {
-  render() {
-    return (
-
-      <StyleRoot>
-        <div className="test" >
-          <div  className="App">
-            <header className="App-header">
-            {/* <FaRocket className="App-logo"/> */}
-            <BsFillEggFill className="App-logo"/>
-            <BsEgg className="App-logo"/>
-              <h1 style={styles.bounceInDown} >Quitado</h1>
-
-            </header>
-          </div>
-        </div>
-      </StyleRoot>
-    );
-  }
-}
-
-
-
 
 export default App;
