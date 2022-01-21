c2ecba243b68ae55fa55fe3880fd04eb858cd8ed
nacho <nacho.mota@gmail.com>
2022-01-21 14:38:21 +0100

    css y condicional

--- src/Circlebox.js
+++ src/Circlebox.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react'
 
-function Circlebox () {
 
+function Stylo (color) {
+  if (color === "ROJO") {
+    return "rojo label"
+  } else if (color === "VERDE") {
+    return "verde label"
+  } else if (color === "AMARILLO") {
+    return "amarillo label"
+  } else {
+    return "azul label"
+  }
+}
+
+
+
+
+function Lista ({ color }) {
+  return (
+    <label className={Stylo(color)}>
+      <input type="radio" value={color} name="color" /> {color}
+    </label>
+  )
+}
+
+
+function Circlebox () {
   const [color, setColor] = useState('')
 
   const onChangeValue = (e) => {
@@ -9,19 +33,14 @@ function Circlebox () {
     setColor(e.target.value)
   }
 
-
-
-  // console.log(dato)
+  const colores = ["ROJO", "VERDE", "AMARILLO", "AZUL"]
 
   return (
     <div>
       <div onChange={onChangeValue}>
-        <input type="radio" value="Rojo" name="color" /> Rojo
-        <input type="radio" value="Amarillo" name="color" /> Amarillo
-        <input type="radio" value="Verde" name="color" /> Verde
+        {colores.map(c => <Lista color={c} key={c} />)}
       </div>
-      <p className="mt-4">El color escojido es: {color}</p>
-
+      <p className="mt-4">El color escojido es: <strong className={Stylo(color)}>{color}</strong></p>
     </div>
   )
 }
