a2a305026d2adf8b7555f290f2db9ede93143400
nacho <nacho.mota@gmail.com>
2022-01-24 18:23:14 +0100

    inicio

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ import 'bootswatch/dist/sandstone/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Menu from './components/Menu'
-import Form1 from './components/Form1'
-import Form2 from './components/Form2'
-import Datos from './components/Datos'
-import Frutas from './components/Frutas';
-import FormAddArray from './components/FormAddArray';
-import FormAddDelToArray from './components/FormAddDelToArray';
-import CryptosFabChecked from './components/CryptosFabChecked'
-import CryptosFabCheckedAddDelArray from './components/CryptosFabCheckedAddDelArray/index'
-
-
+import Login from './components/Login'
 
 class App extends React.Component {
   render () {
@@ -25,16 +16,8 @@ class App extends React.Component {
         <Menu />
         <div className="App-header ">
           <div className="App">
-
             <Routes>
-              <Route excact path="/Form1" element={<Form1 />} />
-              <Route excact path="/Form2" element={<Form2 />} />
-              <Route excact path="/Datos" element={<Datos />} />
-              <Route excact path="/Frutas" element={<Frutas />} />
-              <Route excact path="/FormAddArray" element={<FormAddArray />} />
-              <Route excact path="/FormAddDelToArray" element={<FormAddDelToArray />} />
-              <Route excact path="/CryptosFabChecked" element={<CryptosFabChecked />} />
-              <Route excact path="/CryptosFabCheckedAddDelArray" element={<CryptosFabCheckedAddDelArray />} />
+              <Route excact path="/login" component={Login} />
             </Routes>
           </div>
         </div>
