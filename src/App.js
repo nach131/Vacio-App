551fd5463db345856f7c04a5e0fb33504761702e
nacho <nacho.mota@gmail.com>
2022-02-22 11:16:22 +0100

    no se como estaba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import './App.css';
 import 'bootswatch/dist/sandstone/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,24 +9,24 @@ import Menu from './components/Menu'
 import Login from './components/Login'
 import Form2 from './components/Form2'
 
-class App extends React.Component {
-  render () {
-    return (
-      <BrowserRouter>
-        <Menu />
-        <div className="App-header ">
-          <div className="App">
-            <Routes>
-              <Route excact path="/login" component={<Login />} />
-              <Route excact path="/form2" component={<Form2 />} />
+const App = () => {
 
-            </Routes>
-          </div>
+  return (
+    <BrowserRouter>
+      <Menu />
+      <div className="App-header ">
+        <div className="App">
+          <Routes>
+            <Route excact path="/login" element={<Login />} />
+            <Route excact path="/Form2" element={<Form2 />} />
+
+          </Routes>
         </div>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
 
-    );
-  }
+  );
+
 }
 
 
