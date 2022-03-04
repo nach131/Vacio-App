6a3d50cc03336bd84295686cdd048545c695c2fb
nacho <nacho.mota@gmail.com>
2022-03-04 10:06:58 +0100

    ternario en boton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Lista from './Lista';
 
 function App () {
 
+  const [item, setItem] = useState("nada pulsado")
+
+
+  const ItemClick = (item) => {
+    setItem(item)
+    // console.log(item)
+  }
+
+
 
   return (
     <div className="test" >
       <div className="App">
         <header className="App-header">
-          <Lista />
+          <div class="alert alert-primary" role="alert">
+            {item}
+          </div>
+          <Lista
+            // pulsado={value => setItem(value)}
+            esto={value => setItem(value)}
+            otro={ItemClick}
+          />
         </header>
       </div>
     </div>
--- src/Lista.js
+++ src/Lista.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function Lista () {
+
+// function ItemClick (item) {
+//   console.log(item)
+//   return item
+// }
+
+
+function RenderList ({ otro, item }) {
+  // console.log(otro)
+  return (
+    <>
+      {/* <a href="#alerts" onClick={() => ItemClick(props.item)} className="list-group-item w-50 list-group-item-action">{props.item} </a> */}
+      <a href="#alerts" onClick={() => otro(item)} className="list-group-item w-50 list-group-item-action">{item} </a>
+    </>
+  )
+}
+
+
+function Lista ({ otro }) {
+
+  // console.log(props);
+  // useEffect(() => {
+  //   props.esto("ahora si que si")
+  // })
+
+  const datos = ["Alerts", "Badges", "Breadcrumbs", "Buttons", "Cards", "Modals", "Lists", "Tabs"]
+
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-6"> Examples of list groups in multiple columns. Pros and cons of each scenario. </div>
+
           <div className="col-6">
             {/* <!-- method 1 - sizing classNamees --> */}
             <div className="list-group d-flex flex-row flex-wrap">
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Alerts </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Badges </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Breadcrumbs </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Buttons </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Cards </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Modals </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Lists </a>
-              <a href="#alerts" className="list-group-item w-50 list-group-item-action"> Tabs </a>
+              {datos.map((i, index) => <RenderList item={i} key={index} otro={otro} />)}
             </div>
           </div>
         </div>
@@ -24,5 +44,6 @@ function Lista () {
     </>
   )
 }
+
 
 export default Lista
