9baba7c71d7c62cdd13aed352f3fa5da9426bca3
nacho <nacho.mota@gmail.com>
2022-03-05 23:38:12 +0100

    deploy

---
+++ build/precache-manifest.83c40e7a9945b14d93ea715e4c31b272.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc098b8a9d70b0b0a84891f4d741f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5ca0821c63a00e94eb2",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "925fbd4403db8bf80f57",
+    "url": "/static/css/main.15f86ab5.chunk.css"
+  },
+  {
+    "revision": "b5ca0821c63a00e94eb2",
+    "url": "/static/js/2.e414ca76.chunk.js"
+  },
+  {
+    "revision": "1d4271098918d6c1839a91250cc12457",
+    "url": "/static/js/2.e414ca76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925fbd4403db8bf80f57",
+    "url": "/static/js/main.75b769ed.chunk.js"
+  },
+  {
+    "revision": "14eebc93760149cae84a",
+    "url": "/static/js/runtime-main.0d10fe65.js"
+  }
+]);
