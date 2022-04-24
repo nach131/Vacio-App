92ddd6bd2a8cf8a7214f1450cdcc91c98354a115
nacho <nacho.mota@gmail.com>
2022-04-24 19:57:53 +0200

    react router dom ok

--- src/components/Layouts/Menu.js
+++ src/components/Layouts/Menu.js
@@ -1,38 +1,38 @@
 import React from 'react'
-
-import Navbar from 'react-bootstrap/Navbar'
+import { Outlet, Link } from "react-router-dom";
+import { Navbar, Container } from 'react-bootstrap'
 import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
 
 function Menu () {
   return (
     <>
       <Navbar expand="lg" className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Navbar.Brand href="/">prueba</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="mr-auto my-2 my-lg-0 navbar-dark bg-dark"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/Form1">Form1</Nav.Link>
-            <Nav.Link href="/Form2">Form2</Nav.Link>
-            <Nav.Link href="/Form3">Form3</Nav.Link>
-            <Nav.Link href="/Datos">Datos</Nav.Link>
-            <Nav.Link href="/Frutas">Frutas</Nav.Link>
-            <Nav.Link href="/FormAddArray">FormAddArray</Nav.Link>
-            <Nav.Link href="/FormAddDelToArray">FormAddDelToArray</Nav.Link>
-            <Nav.Link href="/CryptosFabChecked">CryptosFabChecked</Nav.Link>
-            <Nav.Link href="/CryptosFabCheckedAddDelArray">CryptosFabCheckedAddDelArray</Nav.Link>
-            {/* <NavDropdown title="Smart Contracts" id="navbarScrollingDropdown">
+        <Container>
+          <Navbar.Brand href="/">prueba</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="mr-auto my-2 my-lg-0 navbar-dark bg-dark"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to="/Form1">Form1</Nav.Link>
+              <Nav.Link as={Link} to="/Form2">Form2</Nav.Link>
+              <Nav.Link as={Link} to="/Form3">Form3</Nav.Link>
+              <Nav.Link as={Link} to="/Datos">Datos</Nav.Link>
+              <Nav.Link as={Link} to="/Frutas">Frutas</Nav.Link>
+              <Nav.Link as={Link} to="/FormAddArray">FormAddArray</Nav.Link>
+              <Nav.Link as={Link} to="/FormAddDelToArray">FormAddDelToArray</Nav.Link>
+              <Nav.Link as={Link} to="/CryptosFabChecked">CryptosFabChecked</Nav.Link>
+              <Nav.Link as={Link} to="/CryptosFabCheckedAddDelArray">CryptosFabCheckedAddDelArray</Nav.Link>
+              {/* <NavDropdown title="Smart Contracts" id="navbarScrollingDropdown">
               <NavDropdown.Item href="/smart">Add Smart Contracts</NavDropdown.Item>
               <NavDropdown.Item href="/smart_list">List Smart Contracts</NavDropdown.Item>
               <NavDropdown.Divider />
             <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
             </NavDropdown> */}
-          </Nav>
-          {/* <Form className="d-flex">
+            </Nav>
+            {/* <Form className="d-flex">
           <FormControl
             type="search"
             placeholder="Search"
@@ -41,7 +41,8 @@ function Menu () {
           />
           <Button variant="outline-success">Search</Button>
         </Form> */}
-        </Navbar.Collapse>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </>
   )
